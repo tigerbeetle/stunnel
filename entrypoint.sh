82679c1280f36ca285af6786432f549d944ed63f
Federico Lorenzi <florenzi@gmail.com>
2025-09-08 15:14:56 +0200

    add multiple endpoint support

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,13 +4,22 @@ set -eu
 cat > /etc/stunnel/stunnel.conf <<EOF
 foreground = yes
 
-[tigerbeetle client]
+EOF
+
+for i in $(seq 1 ${stunnel_count}); do
+	eval "stunnel_listen=\${stunnel_listen_$i}"
+	eval "stunnel_endpoint=\${stunnel_endpoint_$i}"
+
+	cat >> /etc/stunnel/stunnel.conf <<EOF
+[tigerbeetle client ${i}]
 client     = yes
 accept     = ${stunnel_listen}
 connect    = ${stunnel_endpoint}
 debug      = 5
 PSKsecrets = /etc/stunnel/psk.txt
+
 EOF
+done
 
 cat > /etc/stunnel/psk.txt <<EOF
 ${stunnel_psk}
